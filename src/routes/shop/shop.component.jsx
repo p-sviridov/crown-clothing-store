322b9430fb0c3744d8a71f796ca612f4ce6194dd
Pavel Sviridov <pavlentiysv@gmail.com>
2024-02-01 21:36:30 -0500

    react sagas

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,20 +5,20 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import './shop.styles.scss';
-import { fetchCategoriesAsync,  } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from '../../store/categories/category.action';
 
 const Shop = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCategoriesAsync());
-  }, []);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchCategoriesStart());
+	}, [dispatch]);
 
-  return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=":category" element={<Category />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=":category" element={<Category />} />
+		</Routes>
+	);
 };
 
 export default Shop;
