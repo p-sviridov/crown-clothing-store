44c691ab1645aa5cde5f9f704605a8cb5cdb1194
Pavel Sviridov <pavlentiysv@gmail.com>
2024-03-25 16:24:31 -0400

    A few optimizations

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,25 @@
+import "./cart-item.styles.scss";
+import { FC, memo } from "react";
+import { CartItem as TCartItem } from "../../store/cart/cart.types";
+
+type CartItemProps = {
+  cartItem: TCartItem;
+};
+
+const CartItem: FC<CartItemProps> = memo(({ cartItem }) => {
+  const { name, quantity, imageUrl, price } = cartItem;
+
+  return (
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+});
+
+export default CartItem;
